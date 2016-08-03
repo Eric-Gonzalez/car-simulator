fb3ffc3832324d47b424418cd1d75b4e7ab5621a
Eric Gonzalez <eric@plasticgnome.com>
2016-08-03 09:33:26 -0700

    Get Sensor Image Method

--- remoteApi.py
+++ remoteApi.py
@@ -1,21 +1,33 @@
-import io
-
 import array
+
 import matplotlib.pyplot as plt
 from PIL import Image
+
 import vrep
 
 
 def getRobotSpeed(client):
-    vrep.simxCallScriptFunction(client, 'remoteApiCommandServer', vrep.sim_scripttype_childscript,
-                                'getSpeed', [], [], [], emptyBuff, vrep.simx_opmode_oneshot_wait)
+    print vrep.simxCallScriptFunction(client, 'remoteApiCommandServer',
+                                      vrep.sim_scripttype_childscript,
+                                      'getSpeed', [], [], [], emptyBuff,
+                                      vrep.simx_opmode_oneshot_wait)
 
 
 def setRobotSpeed(client, value):
-    print vrep.simxCallScriptFunction(client, 'remoteApiCommandServer',
-                                      vrep.sim_scripttype_childscript,
-                                      'setSpeed', [], [value], [], emptyBuff,
-                                      vrep.simx_opmode_oneshot_wait)
+    vrep.simxCallScriptFunction(client, 'remoteApiCommandServer',
+                                vrep.sim_scripttype_childscript,
+                                'setSpeed', [], [value], [], emptyBuff,
+                                vrep.simx_opmode_oneshot_wait)
+
+
+def getVisionSensorImage(client):
+    res, visionSensor = vrep.simxGetObjectHandle(clientID, 'vision',
+                                                 vrep.simx_opmode_oneshot_wait)
+    ret, res, data = vrep.simxGetVisionSensorImage(clientID, visionSensor, 0,
+                                                   vrep.simx_opmode_oneshot_wait)
+    imageByteArray = array.array('b', data)
+    image = Image.frombuffer("RGB", res, imageByteArray, "raw", "RGB", 0, 1)
+    return image
 
 
 vrep.simxFinish(-1)  # just in case, close all opened connections
@@ -31,24 +43,17 @@ if clientID != -1:
 
     figure = plt.figure(1)
 
-    setRobotSpeed(clientID, 10)
+    setRobotSpeed(clientID, 8)
 
     first = True
     while True:
         # Fetch Model References
-        res, visionSensor = vrep.simxGetObjectHandle(clientID, 'vision',
-                                                     vrep.simx_opmode_oneshot_wait)
-        ret, res, data = vrep.simxGetVisionSensorImage(clientID, visionSensor, 0,
-                                                       vrep.simx_opmode_oneshot_wait)
-
-        image_byte_array = array.array('b', data)
-        im = Image.frombuffer("RGB", res, image_byte_array, "raw", "RGB", 0, 1)
-
+        image = getVisionSensorImage(clientID)
         if first:
-            plotimg = plt.imshow(im, origin='lower')
+            plotimg = plt.imshow(image, origin='lower')
             first = False
 
-        plotimg.set_data(im)
+        plotimg.set_data(image)
 
         plt.draw()
         plt.pause(0.00000001)
