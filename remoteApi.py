12f4603adf2eda63bee52e7f75e0662b7fb45965
Eric Gonzalez <eric@plasticgnome.com>
2016-08-04 14:24:43 -0700

    Private Methods Fetching Object and Sending Commands

--- remoteApi.py
+++ remoteApi.py
@@ -6,28 +6,41 @@ from PIL import Image
 import vrep
 
 
+# throttle ( 0 -> 1 )
+# brake (0 -> 1)
+# steer (-1 to 1)
+# gps (X, Y, Z)
+# front camera image
+# start Simulation
+# stop Simulation
+
 def getRobotSpeed(client):
-    print vrep.simxCallScriptFunction(client, 'remoteApiCommandServer',
-                                      vrep.sim_scripttype_childscript,
-                                      'getSpeed', [], [], [], emptyBuff,
-                                      vrep.simx_opmode_oneshot_wait)
+    return _simSendCommand(client, 'getSpeed')
 
 
 def setRobotSpeed(client, value):
-    vrep.simxCallScriptFunction(client, 'remoteApiCommandServer',
-                                vrep.sim_scripttype_childscript,
-                                'setSpeed', [], [value], [], emptyBuff,
-                                vrep.simx_opmode_oneshot_wait)
+    return _simSendCommand(client, 'setSpeed', inputFloats=[value])
 
 
-def getVisionSensorImage(client):
-    res, visionSensor = vrep.simxGetObjectHandle(clientID, 'vision',
-                                                 vrep.simx_opmode_oneshot_wait)
+def getVisionSensorImage(clientID):
+    res, visionSensor = _simGetObject(clientID, 'front_camera')
     ret, res, data = vrep.simxGetVisionSensorImage(clientID, visionSensor, 0,
                                                    vrep.simx_opmode_oneshot_wait)
     imageByteArray = array.array('b', data)
     image = Image.frombuffer("RGB", res, imageByteArray, "raw", "RGB", 0, 1)
     return image
+
+
+def _simGetObject(clientID, objectName):
+    return vrep.simxGetObjectHandle(clientID, objectName, vrep.simx_opmode_oneshot_wait)
+
+
+def _simSendCommand(clientID, functionName, inputInts=[], inputFloats=[], inputStrings=[],
+                    byteBuffer=bytearray()):
+    return vrep.simxCallScriptFunction(clientID, 'remoteApiCommandServer',
+                                       vrep.sim_scripttype_childscript, functionName, inputInts,
+                                       inputFloats, inputStrings, byteBuffer,
+                                       vrep.simx_opmode_oneshot_wait)
 
 
 vrep.simxFinish(-1)  # just in case, close all opened connections
