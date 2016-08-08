91f501f6a67c5adff60096e4a9ce18113fbc70a5
Eric Gonzalez <eric@plasticgnome.com>
2016-08-07 17:15:53 -0700

    Recorder Class

--- api/remoteApi.py
+++ api/remoteApi.py
@@ -1,7 +1,8 @@
-from api.car import Car
-from api.simulator import Simulator
-import matplotlib.pyplot as plt
 import time
+
+from api.car import Car
+from api.recorder import Recorder
+from api.simulator import Simulator
 
 simulator = Simulator()
 car = Car(simulator)
@@ -10,15 +11,16 @@ simulator.connect()
 
 simulator.start()
 
-print car.set_throttle(.1)
+print car.set_throttle(.3223)
 print car.get_throttle()
-print car.set_steering_angle(.66)
+print car.set_steering_angle(.1)
 print car.get_steering_angle()
 
 duration = time.time() + 10
-plot_image = plt.imshow(car.get_front_camera_image(), origin='lower')
-while time.time() < duration:
-    # Display Fetched Image
-    plot_image.set_data(car.get_front_camera_image())
-    plt.draw()
-    plt.pause(.0001)
+recorder = Recorder(car)
+try:
+    recorder.start()
+    time.sleep(5)
+finally:
+    recorder.stop()
+    simulator.stop()
